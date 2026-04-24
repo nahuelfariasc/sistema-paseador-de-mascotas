d450bba714020e9ebc10e72ca203486b4ddcdade
Nahuel Farias <nahuelfariasc@gmail.com>
2026-04-23 21:51:14 -0300

    unificar base de datos

--- database/sistema_paseador_mascotas.sql
+++ database/sistema_paseador_mascotas.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Apr 23, 2026 at 09:28 PM
--- Server version: 8.0.40
--- PHP Version: 8.3.14
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2026 at 02:40 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,10 +28,24 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `clientes` (
-  `idCliente` int NOT NULL,
-  `nombre` varchar(20) DEFAULT NULL,
+  `idCliente` int(11) NOT NULL,
+  `nombre` varchar(20) NOT NULL DEFAULT 'NULL',
   `telefono` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `direcciones`
+--
+
+CREATE TABLE `direcciones` (
+  `idDirección` int(20) NOT NULL,
+  `IdCliente` int(20) NOT NULL,
+  `calle` varchar(20) NOT NULL,
+  `altura` int(4) NOT NULL,
+  `depto` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Indexes for dumped tables
@@ -44,6 +58,15 @@ ALTER TABLE `clientes`
   ADD PRIMARY KEY (`idCliente`);
 
 --
+-- Indexes for table `direcciones`
+--
+ALTER TABLE `direcciones`
+  ADD PRIMARY KEY (`idDirección`),
+  ADD KEY `IdCliente` (`IdCliente`),
+  ADD KEY `IdCliente_2` (`IdCliente`),
+  ADD KEY `IdCliente_3` (`IdCliente`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -51,7 +74,23 @@ ALTER TABLE `clientes`
 -- AUTO_INCREMENT for table `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `idCliente` int NOT NULL AUTO_INCREMENT;
+  MODIFY `idCliente` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `direcciones`
+--
+ALTER TABLE `direcciones`
+  MODIFY `idDirección` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `direcciones`
+--
+ALTER TABLE `direcciones`
+  ADD CONSTRAINT `cliente_direccion` FOREIGN KEY (`IdCliente`) REFERENCES `clientes` (`idCliente`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
